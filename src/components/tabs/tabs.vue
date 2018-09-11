fe2eaf4a37f6e56da292f6ade8265bf0f0e1fb06
lwc <wence.lv@cnlaunch.com>
2018-09-11 10:06:38 +0800

    项目构建

---
+++ src/components/tabs/tabs.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="tabs">
+    <div class="nav_body">
+      <div class="nav_mid">
+        <nav class="nav_tab">
+          <ul>
+            <li>
+              <router-link to="/personalAssets">
+                <span></span>
+                <p>个人资产</p>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/favorite">
+                <span></span>
+                <p>收藏夹</p>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/orderHistory">
+                <span></span>
+                <p>历史订单</p>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/securityCenter">
+                <span></span>
+                <p>安全中心</p>
+              </router-link>
+            </li>
+          </ul>
+        </nav>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {baseURL,cardURL} from '@/common/js/public.js';
+  export default {
+    name: "tabs",
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+  }
+</script>
+
+<style scoped lang="stylus">
+  .nav_body{
+    background-color: #f3f3f3;
+    padding-top 20px
+  }
+  .nav_mid{
+    width: 1212px;
+    margin:0 auto;
+  }
+  .nav_tab{
+    font-size: 18px;
+    width: 100px;
+    height: 128px;
+    float: left;
+  }
+  .nav_tab p{
+    color: #222222;
+    line-height: 34px;
+  }
+  .nav_tab span{
+    width: 14px;
+    height: 14px;
+    background-color: #7d7d7d;
+    display: inline-block;
+    float: left;
+    margin-top: 10px;
+    margin-right: 10px;
+  }
+</style>
---
+++ src/components/toggle/toggle.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="toggle">
+    <ul>
+      <li v-for="(item,index) of toggleParam" @click="toggle(index)" :class="{active:index===toggleIndex}">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "toggle",
+    components: {},
+    data() {
+      return {
+        toggleParam: ["交易平台", "转让平台"],
+      }
+    },
+    props: {
+      toggleIndex: {
+        type: Number,
+        default: 0
+      },
+    },
+    created() {
+    },
+    mounted() {
+    },
+    watch: {},
+    computed: {},
+    methods: {
+      toggle(index) {
+        if (index === 0) {
+          this.$router.push("/home")
+        } else if (index === 1) {
+/*          if(JSON.parse(sessionStorage.getItem("loginInfo"))&&JSON.parse(sessionStorage.getItem("userName"))){
+            let info={};
+            info.loginInfo=JSON.parse(sessionStorage.getItem("loginInfo"));
+            info.userName=JSON.parse(sessionStorage.getItem("userName"));
+            console.log(info);
+            let popup = window.open('http://10.0.0.123:5001',"title");
+            setTimeout(function () {
+              popup.postMessage(info, 'http://10.0.0.123:5001');
+            }, 2000);
+          }else{
+            window.open('http://10.0.0.123:5001')
+          }*/
+          window.open('http://47.92.98.66:5001')
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped lang="stylus">
+  .toggle {
+    width 100%
+    border 1px solid #d91e01
+    background-color #d91e01
+    ul {
+      width 1212px
+      margin 0 auto
+      font-size 0
+      padding-left 34px
+      li {
+        display inline-block
+        margin-right 20px
+        text-align center
+        width 106px
+        height 48px
+        line-height 48px
+        font-size: 16px;
+        color: #f3f3f3;
+        cursor pointer
+      }
+      li:active {
+        background-color #ffffff
+        color: #d91e01;
+      }
+      .active {
+        background-color #ffffff
+        color: #d91e01;
+      }
+    }
+  }
+
+</style>
