da58a58bf3fbab49c8e2dbaa3f4b93d220f2d23b
zhangzhenchang <591781589@qq.com>
2018-09-20 18:58:54 +0800

    修改文字滚动

--- src/App.vue
+++ src/App.vue
@@ -131,11 +131,9 @@
           <button type="button" @click="caseSearch">搜索</button>
           <div style="clear: both"></div>
           <span class="search-tips">
-            <marquee behavior="scroll" direction="up" scrollamount="3" scrolldelay="200" height="16" onmouseover=this.stop() onmouseout=this.start()>
-              <p>百万积分大派送！</p>
-              <p>百万积分大派送， 今秋送好礼！</p>
-              <p>百万积分大派送， 微信支付送等额可信积分！</p>
-            </marquee>
+            <ul id="con1" ref="con1" :class="{anim:animate==true}">
+              <li v-for='item in items'>{{item.name}}</li>
+            </ul>
           </span>
         </div>
       </div>
@@ -460,7 +458,16 @@
           label: '1000'
         },],
         numbervalue: '',
+        animate:false,
+        items:[
+          {name:"百万积分大派送！"},
+          {name:"百万积分大派送， 今秋送好礼！"},
+          {name:"百万积分大派送， 微信支付送等额可信积分！"}
+        ]
       }
+    },
+    created(){
+      setInterval(this.scroll,2000)
     },
     mounted() {
       if (JSON.parse(sessionStorage.getItem("loginInfo"))) {
@@ -490,6 +497,14 @@
       },
     },
     methods: {
+      scroll(){
+        this.animate=true;    // 因为在消息向上滚动的时候需要添加css3过渡动画，所以这里需要设置true
+        setTimeout(()=>{      //  这里直接使用了es6的箭头函数，省去了处理this指向偏移问题，代码也比之前简化了很多
+          this.items.push(this.items[0]);  // 将数组的第一个元素添加到数组的
+          this.items.shift();               //删除数组的第一个元素
+          this.animate=false;  // margin-top 为0 的时候取消过渡动画，实现无缝滚动
+        },500)
+      },
       //搜索诊断报告
       reportSearch() {
         this.$set(this.$refs.radioTop,'checked',true)
@@ -1391,5 +1406,19 @@
       }
     }
   }
-
+  #box{
+    width: 300px;
+    height: 32px;
+    overflow: hidden;
+    padding-left: 30px;
+    border: 1px solid black;
+  }
+  .anim{
+    transition: all 0.5s;
+    margin-top: -30px;
+  }
+  #con1 li{
+    list-style: none;
+    height: 30px;
+  }
 </style>
