552cf3149d4a691e13e941f3a3620f2cf5e25fad
lwc <wence.lv@cnlaunch.com>
2018-11-06 14:18:18 +0800

    修改接口地址

--- src/common/js/public.js
+++ src/common/js/public.js
@@ -1,8 +1,8 @@
-let baseURL="https://wallet-api.launchain.org";
+let baseURL="https://wallet-api.launchain.cn";
 let loginPlatform="https://login.datajs.com.cn";
 let exchangePlatform="https://exchange.datajs.com.cn";
 let transferPlatform="https://transfer.datajs.com.cn";
 let searchPlatform="https://search.datajs.com.cn";
-let bigDataURL="https://search-api.launchain.org";
+let bigDataURL="https://search-api.launchain.cn";
 export {baseURL,loginPlatform,exchangePlatform,transferPlatform,searchPlatform,bigDataURL}
 
