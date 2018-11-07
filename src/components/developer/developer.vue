a1b7e714cd50eff7d73a86e9155060a8ded82f65
吕文策 <23797610@qq.com>
2018-11-07 22:51:28 +0800

    开发者计划

--- src/components/developer/developer.vue
+++ src/components/developer/developer.vue
@@ -7,12 +7,33 @@
           <div class="presentation">
             <h4>Engine API 白皮书</h4>
             <p>介绍了平台所提供的API接口，主要分为 HTTP 和 gRPC 两类，用于完成数据的上链存证、数据获取和查询、数据所在区块信息等操作。</p>
-            <a href="javascript:void(0)">下载</a>
+            <!--<a href="javascript:void(0)">下载</a>-->
           </div>
         </li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <img src="./images/2.png" alt="">
+          <div class="presentation">
+            <h4>元数据标准白皮书</h4>
+            <p>定义了平台中数据实体形式和元数据内容，主要包括了标识信息、元数据信息、位置信息、自定义列表。</p>
+            <!--<a href="javascript:void(0)">下载</a>-->
+          </div>
+        </li>
+        <li>
+          <img src="./images/3.png" alt="">
+          <div class="presentation">
+            <h4>多方计算白皮书</h4>
+            <p>一组互不信任的参与方，在不泄露彼此数据的前提下，进行协同计算，得到计算结果。</p>
+            <!--<a href="javascript:void(0)">下载</a>-->
+          </div>
+        </li>
+        <li>
+          <img src="./images/4.png" alt="">
+          <div class="presentation">
+            <h4>安全上链白皮书</h4>
+            <p>基于硬件安全元素的敏感数据保护方案，支持嵌入式设备及pc等多种接口。</p>
+            <!--<a href="javascript:void(0)">下载</a>-->
+          </div>
+        </li>
       </ul>
     </div>
   </div>
@@ -39,14 +60,23 @@
 .developer{
   .dev{
     width 1212px
-    height 600px
-    background-color bisque
     margin 0 auto
     ul{
+      padding-top 56px
+      padding-bottom 85px
       li{
+        position relative
+        box-sizing border-box
+        height 172px
+        padding-top 26px
+        margin-bottom 13px
         img{
           width 142px
           height 172px
+          position absolute
+          top 0
+          left 44px
+          z-index 1000
         }
         .presentation{
           box-sizing border-box
@@ -59,6 +89,7 @@
           position relative
           padding-top 22px
           padding-left 46px
+          margin-left 135px
           h4{
             
             font-size: 20px;
